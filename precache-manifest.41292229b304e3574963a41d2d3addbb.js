79f593b1071b7f477b86ae8152746751ed5c67f9
amrit <amritpalsoftware@gmail.com>
2021-12-29 13:28:19 +0530

    initial commit

---
+++ precache-manifest.41292229b304e3574963a41d2d3addbb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f77fc5b0c02f3210b358d109060c6fd8",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "ad454feaa27e0553ee4c",
+    "url": "/home/static/css/main.956fad23.chunk.css"
+  },
+  {
+    "revision": "e42b8e6c924a6200b9f3",
+    "url": "/home/static/js/2.ecd9d33a.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/home/static/js/2.ecd9d33a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad454feaa27e0553ee4c",
+    "url": "/home/static/js/main.3b743401.chunk.js"
+  },
+  {
+    "revision": "c0578b487099793a068c",
+    "url": "/home/static/js/runtime-main.3c590061.js"
+  },
+  {
+    "revision": "32f6b574eb11519020e24620a38bdf55",
+    "url": "/home/static/media/hashirshoaeb.32f6b574.png"
+  },
+  {
+    "revision": "9d3da0e22086c40d5e3106d01c0f6402",
+    "url": "/home/static/media/resume.9d3da0e2.pdf"
+  }
+]);
